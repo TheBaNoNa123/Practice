8084d0725c9d3ded59c0d29b0f3b5c48a03f4046
TheBaNoNa123 <rohan.singh42351@gmail.com>
2026-06-27 13:28:31 -0700

    Update Repository.java
    Created method to check for duplicate user and login authentication

--- code/src/main/java/com/register/code/Repository.java
+++ code/src/main/java/com/register/code/Repository.java
@@ -2,5 +2,10 @@ package com.register.code;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface Repository extends JpaRepository<userEntity, Integer> {
+    Optional<userEntity> findByName(String name);
+    boolean existsByName(String name);
+
 }
