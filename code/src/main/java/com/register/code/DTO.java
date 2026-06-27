3b4afb4b9496d200b7ae2f1b6c3ce697547feb74
TheBaNoNa123 <rohan.singh42351@gmail.com>
2026-06-27 13:28:56 -0700

    Update DTO.java
    Added rules to username and password

--- code/src/main/java/com/register/code/DTO.java
+++ code/src/main/java/com/register/code/DTO.java
@@ -1,7 +1,18 @@
 package com.register.code;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
 public class DTO {
+    @NotBlank(message = "Field cannot be empty")
+    @Size(min = 5, max = 16, message = "Username must contain between 5 and 16 characters")
     private String username;
+
+    @NotBlank(message = "Field cannot be empty")
+    @Size(min = 5, max = 12, message = "Passwords must contain between 5 and 12 characters")
     private String password;
 
     public String getUsername(){
